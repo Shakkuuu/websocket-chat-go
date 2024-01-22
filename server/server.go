d35031fa11e8fac8f9dafb9dce67bcfe248b4d9e
Shakkuuu <shakku0521@gmail.com>
2024-01-22 14:35:07 +0900

    リクエストログのためのmiddleを実装、htmlのhead修正、アイコン追加

--- server/server.go
+++ server/server.go
@@ -5,21 +5,32 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"websocket-chat/controller"
 
 	"golang.org/x/net/websocket"
 )
 
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		next.ServeHTTP(w, r)
+
+		log.Printf("・[%s] %s %s %s\n", r.Method, r.RemoteAddr, r.URL, time.Since(start))
+	})
+}
+
 func Init(port string, view embed.FS) {
 	http.Handle("/view/", http.FileServer(http.FS(view)))
 
-	http.HandleFunc("/", controller.Index)                             // indexページ
-	http.HandleFunc("/room", controller.Room)                          // Room内のページ
-	http.HandleFunc("/rooms", controller.RoomsList)                    // Room一覧取得
-	http.HandleFunc("/users", controller.RoomUsersList)                // User一覧取得
-	http.Handle("/ws", websocket.Handler(controller.HandleConnection)) // メッセージWebsocket用
-	go controller.HandleMessages()                                     // goroutineとチャネルで常にメッセージを待つ
+	http.Handle("/", loggingMiddleware(http.HandlerFunc(controller.Index)))              // indexページ
+	http.Handle("/room", loggingMiddleware(http.HandlerFunc(controller.Room)))           // Room内のページ
+	http.Handle("/rooms", loggingMiddleware(http.HandlerFunc(controller.RoomsList)))     // Room一覧取得
+	http.Handle("/users", loggingMiddleware(http.HandlerFunc(controller.RoomUsersList))) // User一覧取得
+	http.Handle("/ws", websocket.Handler(controller.HandleConnection))                   // メッセージWebsocket用
+	go controller.HandleMessages()                                                       // goroutineとチャネルで常にメッセージを待つ
 
 	// サーバ起動
 	err := http.ListenAndServe(port, nil)
